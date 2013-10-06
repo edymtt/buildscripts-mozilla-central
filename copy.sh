68d68dd50647bb1f278661e28e0e2ba5bf0fe493
Eric Miotto <edymtt@netscape.net>
2013-10-06 17:57:43 +0200

    Initial changes to copy.sh to mount automatically the dmg

--- copy.sh
+++ copy.sh
@@ -1,2 +1,48 @@
 rm -fr ~/Applications/Nightly.app
-cp -av obj-firefox/dist/Nightly.app ~/Applications/
+
+for IMAGE in obj-firefox/dist/*.dmg
+do
+	echo $IMAGE
+
+	MOUNTPOINT=""
+	DEVPATH=""
+    hdiutil attach "$IMAGE" | {
+    	while read REPLY; do
+        echo $REPLY
+        echo `expr index "$REPLY" expected`
+        if [[ `expr index "$REPLY" expected` != 0 ]]
+        	then
+        	continue
+        else
+         if [[ $DEVPATH == "" ]]
+         	then
+         	DEVPATH = `echo $REPLY | awk "print $0"`
+         fi
+         if [[ $MOUNTPOINT == "" ]]
+         	then
+         	MOUNTPOINT = `echo $REPLY | awk "print $3"`
+         fi
+
+         if [[ $MOUNTPOINT != "" && $DEVPATH != "" ]]
+         	then
+         	break
+         fi
+        fi
+    done
+}
+
+    if [[ $MOUNTPOINT == "" || $DEVPATH == "" ]]
+    	then
+    	echo "Error in finding where hdiutil mounted the IMAGE, MOUNTPOINT = $MOUNTPOINT, DEVPATH =$DEVPATH"
+    	exit 1
+    fi
+	
+	if [ -e $MOUNTPOINT]
+		then
+	  cp -av $MOUNTPOINT/Nightly.app ~/Applications/
+
+	  hdiutil detach $DEVPATH
+	fi
+done
+
+#cp -av obj-firefox/dist/Nightly.app ~/Applications/
